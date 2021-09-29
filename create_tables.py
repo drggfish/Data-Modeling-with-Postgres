536da6dea294738b9eb3b13de8ec09a464acbcc2
Greg Fisher <drggfish@gmail.com>
2021-09-29 08:43:05 -0400

    issue resolution 1

--- create_tables.py
+++ create_tables.py
@@ -9,16 +9,16 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
+    # conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    # conn.set_session(autocommit=True)
+    # cur = conn.cursor()
     
-    # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    # # create sparkify database with UTF8 encoding
+    # cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    # cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
-    conn.close()    
+    #conn.close()    
     
     # connect to sparkify database
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
@@ -60,7 +60,7 @@ def main():
     """
     cur, conn = create_database()
     
-    drop_tables(cur, conn)
+    #drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
