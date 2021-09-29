bfe42ecae44631a11a915c9759c7df64f3d3bf72
Greg Fisher <drggfish@gmail.com>
2021-09-29 09:48:01 -0400

    progress

--- sql_queries.py
+++ sql_queries.py
@@ -8,27 +8,27 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-# songplay_table_create = ("""
-# CREATE TABLE IF NOT EXISTS songplays (
-#         songplay_id varchar, 
-#         start_time timestamp, 
-#         user_id varchar, 
-#         level varchar, 
-#         song_id varchar, 
-#         artist_id varchar, 
-#         session_id varchar, 
-#         location varchar, 
-#         user_agent varchar);
-# """)
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id varchar PRIMARY KEY, 
+        start_time timestamp REFERENCES time(start_time), 
+        user_id varchar NOT NULL REFERENCES users(user_id), 
+        level varchar, 
+        song_id varchar REFERENCES songs(song_id), 
+        artist_id varchar REFERENCES artists(artist_id), 
+        session_id varchar, 
+        location varchar, 
+        user_agent varchar);
+""")
 
-# user_table_create = ("""
-# CREATE TABLE IF NOT EXISTS users (
-#         user_id int, 
-#         first_name varchar, 
-#         last_name varchar, 
-#         gender varchar, 
-#         level varchar);
-# """)
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+        user_id varchar PRIMARY KEY, 
+        first_name varchar, 
+        last_name varchar, 
+        gender varchar, 
+        level varchar);
+""")
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
@@ -61,22 +61,24 @@ CREATE TABLE IF NOT EXISTS time (
 
 # INSERT RECORDS
 
-# songplay_table_insert = ("""
-# INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-#     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-#     ON CONFLICT (songplay_id) DO NOTHING;
-# """)
+songplay_table_insert = ("""
+INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING;
+""")
 
-# user_table_insert = ("""
-# INSERT INTO users
-#     (user_id, first_name, last_name, gender, level)
-#     VALUES (%s, %s, %s, %s, %s);
-# """)
+user_table_insert = ("""
+INSERT INTO users
+    (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO NOTHING;
+""")
 
 song_table_insert = ("""
 INSERT INTO songs 
         (song_id, title, artist_id, year, duration) 
-        VALUES (%s, %s, %s, %s, %s);
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
@@ -96,18 +98,15 @@ INSERT INTO time
 
 # # FIND SONGS
 
-# song_select = ("""
-# SELECT song_id, artists.artist_id
-#     FROM songs JOIN artists ON songs.artist_id = artists.artist_id
-#         WHERE songs.title = %s
-#         AND artists.name = %s
-#         AND songs.duration = %s
-# """)
+song_select = ("""
+SELECT song_id, artists.artist_id
+    FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+        WHERE songs.title = %s
+        AND artists.name = %s
+        AND songs.duration = %s
+""")
 
 # QUERY LISTS
 
-# create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-# drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-
-create_table_queries = [song_table_create, artist_table_create,time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create,time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
