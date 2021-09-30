9e4ae8c4aa276ffa3becaf0eea343d94d82c4525
Greg Fisher <drggfish@gmail.com>
2021-09-30 13:26:41 -0400

    Final edits

--- etl.py
+++ etl.py
@@ -4,6 +4,7 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+
 def process_song_file(cur, filepath):
     """" Function used to process song file data
          Arguments:
@@ -17,11 +18,13 @@ def process_song_file(cur, filepath):
 
     for index, row in df.iterrows():
         # insert song record
-        song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
+        song_data = df[['song_id', 'title', 'artist_id', 'year',
+                        'duration']].values[0].tolist()
         cur.execute(song_table_insert, song_data)
-    
+
         # insert artist record
-        artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
+        artist_data = df[['artist_id', 'artist_name', 'artist_location',
+                          'artist_latitude', 'artist_longitude']].values[0].tolist()
         cur.execute(artist_table_insert, artist_data)
 
 
@@ -40,14 +43,16 @@ def process_log_file(cur, filepath):
     df = df[df['page']=='NextSong']
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'], unit='ms') 
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
     time_data = []
     for ts in t:
-        time_data.append([ts, ts.hour, ts.day, ts.week, ts.month, ts.year, ts.day_name()])
-    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
-    
+        time_data.append([ts, ts.hour, ts.day, ts.week, ts.month,
+                          ts.year, ts.day_name()])
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month',
+                     'year', 'weekday')
+
     time_df = pd.DataFrame.from_records(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
@@ -62,26 +67,19 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = [pd.to_datetime(row["ts"], 
-                     unit='ms'), 
-                     row.userId, 
-                     row.level, 
-                     songid, 
-                     artistid, 
-                     row.sessionId, 
-                     row.location, 
-                     row.userAgent]
+        songplay_data = [pd.to_datetime(row["ts"], unit='ms'), row.userId,
+                         row.level, songid, artistid, row.sessionId,
+                         row.location, row.userAgent]
 
         cur.execute(songplay_table_insert, songplay_data)
 
@@ -91,7 +89,7 @@ def process_data(cur, conn, filepath, func):
          the log data or she song data directories
          Arguments:
            cur: the cursor object to the sparkifydb
-           conn: the connection object to the sparkifydb used to 
+           conn: the connection object to the sparkifydb used to
            filepath: the path to the file data directory
            func: the function to call that has been passed in from Main
         Returns:
@@ -101,7 +99,7 @@ def process_data(cur, conn, filepath, func):
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -116,16 +114,18 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    """" Main function used to run work flow.  Initially it sets up the connection object to the
-         sparkifydb. Subsequently it makes two calls to the process_data function to process the
-         song and log data files.
+    """" Main function used to run work flow.  Initially it sets up the
+         connection object to the sparkifydb. Subsequently it makes two
+         calls to the process_data function to process the song and log
+         data files.
          Arguments:
            None
         Returns:
            None
     """
-    
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student \
+                            password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
